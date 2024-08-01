637129553586c21c8c5b556cdc93213783f4e396
shravanighule <shravanighule16@gmail.com>
2024-08-01 10:55:26 +0530

    Add files via upload

---
+++ Exp_3d_reversing_number (1).cpp
@@ -0,0 +1,24 @@
+//shravanighule 23070123056
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int prn,a;
+
+    cout<<"Enter yout prn: ";
+    cin>>prn;
+
+    while(prn>0)
+    {
+        a = prn%10;         //modulus of the input by 10 and storing the result in a.
+        prn = prn/10;       //dividing the remaining number by 10 and storing it again in the input prn.
+        cout<<a;
+    }
+}
+
+Enter yout prn: 123056
+650321
+--------------------------------
+Process exited after 9.65 seconds with return value 0
+Press any key to continue . . .
